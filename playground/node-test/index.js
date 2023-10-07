a6769d2b02f846bba13ab69fdbf8d71c07ca0c75
Alessio Orsini <35498861+orsett0@users.noreply.github.com>
2023-10-07 20:31:57 +0200

    Better handling of filesystem in node-test.

--- playground/node-test/index.js
+++ playground/node-test/index.js
@@ -1,21 +1,28 @@
 'use strict';
 const { readFile } = require('node:fs/promises');
+const { realpathSync } = require('node:fs'); 
 const { WASI } = require('wasi');
 const { argv, env } = require('node:process');
-const { join } = require('node:path');
+const { join, dirname, basename } = require('node:path');
+
+const WASMpath = realpathSync('../../target/wasm32-wasi/debug/lofty-wasm.wasm');
+
+var file_rp = realpathSync(argv.at(-1));
+var directory = dirname(file_rp);
+var filename = basename(file_rp);
 
 const wasi = new WASI({
   version: 'preview1',
-  args: argv.slice(1, argv.length),
+  args: argv.slice(1, argv.length - 1).concat([join('/sandbox', filename)]),
   env,
   preopens: {
-    '/sandbox': '.',
+    '/sandbox': directory,
   },
 });
 
 (async () => {
   const wasm = await WebAssembly.compile(
-    await readFile(join(__dirname, '../../target/wasm32-wasi/debug/lofty-wasm.wasm')),
+    await readFile(WASMpath),
   );
   const instance = await WebAssembly.instantiate(wasm, wasi.getImportObject());
 
